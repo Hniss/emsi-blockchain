19f6fe5b80e90d8951e0b178bfd50ce3a4ff6fd9
Hniss <nissoul.hamza@gmail.com>
2021-03-30 01:17:30 +0100

    fourth Tasks

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -1,3 +1,4 @@
+#define HBLK_CRYPTO_H
 #include <openssl/sha.h>
 #include <stdint.h>
 #include <openssl/ec.h>
@@ -30,3 +31,5 @@ void _print_hex_buffer(uint8_t const *buf, size_t len);
 EC_KEY *ec_create(void);
 
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
+
+EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
