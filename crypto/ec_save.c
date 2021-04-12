9bdc292bc028bf6ce1fdba7813571b862ccd2a63
hniss <nissoul.hamza@gmail.com>
2021-04-12 18:13:14 +0000

    save file

---
+++ crypto/ec_save.c
@@ -0,0 +1,63 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_from_pub - generates EC_KEY from pub key
+ * @pub: the pub key in buffer
+ * Return: pointer to generated EC_KEY struct or NULL
+ */
+
+int ec_save(EC_KEY *key, char const *folder){
+	char path[BUFSIZ];
+	
+	int dir_err = -1;
+	
+	struct stat sb;
+	FILE *f;
+	
+	if(stat(folder, &sb))
+	{
+		dir_err = mkdir(folder, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+	
+		
+		if(dir_err == -1)
+		{
+			return 0;
+		}
+	}
+	
+	sprintf(path, "%s/%s", folder, PUB_FILENAME);
+	f = fopen(path, "w");
+
+	if(!f)
+	{
+		return 0;
+	}
+
+	if(!PEM_write_EC_PUBKEY(f, key))
+	{
+		fclose(f);
+		return 0;
+	}
+	
+	fclose(f);
+	
+	sprintf(path, "%s/%s", folder, PRI_FILENAME);
+	
+	f = fopen(path, "w");
+	
+	if(!f)
+	{
+		return 0;
+	}
+
+
+	if(!PEM_write_ECPrivateKey(f, key, NULL, NULL, 0, NULL, NULL))
+	{
+		fclose(f);
+		return 0;
+	}
+	
+	fclose(f);
+	return 1;
+
+}
