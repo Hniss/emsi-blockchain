e02ee72de1bbc335e78c825fc3dea6daf67df6e8
hniss <nissoul.hamza@gmail.com>
2021-04-12 17:42:04 +0000

    editing

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,16 +1,13 @@
 #include "hblk_crypto.h"
-#include <openssl/sha.h>
 
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]){
-	SHA256_CTX context;
-	int i = 0;
-	if(!digest)
-		return NULL;
-	
-	SHA256_Init(&context);
-	for(i =0; i<20; i++)
-	SHA256_Update(&context, s, len);
-	SHA256_Final(digest ,&context);
-	
-	return digest;
+/**
+ * sha256 - compute the hash of a sequence of bytes
+ */
+uint8_t* sha256(int8_t const* s, size_t len,
+	uint8_t digest[SHA256_DIGEST_LENGTH])
+{
+	if (!s || !digest)
+		return (NULL);
+	return (SHA256((const unsigned char*)s, len, digest));
 }
+
